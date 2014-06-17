8c3b54b26b22eb33497d398637d0e7797888bf3d
sol0 <andrew.barsukov@gmail.com>
2014-06-18 02:30:08 +0300

    Remove func added, some cosmetic changes

--- app/controllers/Events.scala
+++ app/controllers/Events.scala
@@ -6,22 +6,26 @@ import models.Event
 import java.util.Date
 
 object Events extends Controller {
-	def list = Action{
-	  implicit request=>
-	    val events=Event.getAll
-	    //TODO добавить ссылку на темплейт со списком
-	    Ok(views.html.events.list(events))
-	}
-	def show(id:Long)=Action{
-	  implicit request=>
-	    val e=Event.getById(id)
-	    e match{
-	      case None=>NotFound
-	      case Some(x)=>Ok(views.html.events.details(x))
-	    }
-	}
-	def add=Action{
-	  implicit request =>
-	  Redirect(routes.Events.list())
-	}
+  def list = Action {
+    implicit request =>
+      val events = Event.getAll
+      Ok(views.html.events.list(events))
+  }
+  def show(id: Long) = Action {
+    implicit request =>
+      val e = Event.getById(id)
+      e match {
+        case None => NotFound
+        case Some(x) => Ok(views.html.events.details(x))
+      }
+  }
+  def add = Action {
+    implicit request =>
+      Redirect(routes.Events.list())
+  }
+  def remove(id: Long) = Action {
+    implicit request =>
+    Event.removeById(id)
+    Redirect(routes.Events.list())
+  }
 }
